a5e2d0e2d8deae6d74dac735d2c9354741ee0032
Sundararajan <msundararajan@lm-maa-22017453.paypalcorp.com>
2019-02-06 15:33:57 +0530

    accesing static resource

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app=express();
 app.listen(3000);
 console.log('node listening to 3000');
 
+app.use(express.static(__dirname+'/public'));
+
 app.get('/',(req,res)=>{
   console.log('root called');
   res.send('ji');
