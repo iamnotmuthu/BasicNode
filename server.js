9f53fcbf83ee60b775e5b095109bc01ce676ec9f
Muthukumar Sundararajan <iamnotmuthu@gmail.com>
2019-02-06 22:09:05 +0530

    redirect to static resource

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var express=require('express');
 var app=express();
 app.listen(3000);
 console.log('node listening to 3000');
-
-app.use(express.static(__dirname+'/public'));
+var staticPath=__dirname+'/public';
+console.log(`static path is ${staticPath}`);
+app.use(express.static(__dirname +'/public'));//REquired this line, so that we can access static file directly from browser
 
 app.get('/',(req,res)=>{
   console.log('root called');
-  res.send('ji');
+  res.sendFile(`${staticPath}/help.html`);//send file takes absolute path for static files. internal redirection, so need abs path.
 });
